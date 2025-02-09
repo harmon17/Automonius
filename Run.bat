073a821a71ccd9453828904ec6780f0eff91400f
harmon17 <hmesa28@gmail.com>
2025-02-09 16:46:23 +0800

    Fixing the modules/path

--- Run.bat
+++ Run.bat
@@ -1,3 +1,18 @@
 @echo off
-java -jar C:\Users\hmesa\Documents\Automonius\target\Automonius-1.0-SNAPSHOT.jar
+
+:: Set the path to your JavaFX SDK
+set JAVAFX_PATH="C:\Users\hmesa\Documents\javafx-sdk-23.0.2\lib"
+
+:: Set the path to your JDK
+set JAVA_HOME="C:\Program Files\Java\jdk-22"
+
+:: Use the appropriate version of java (make sure it's JDK 22 or your desired version)
+set PATH=%JAVA_HOME%\bin;%PATH%
+
+:: Set the path to your JAR file
+set JAR_PATH="Automonius\target\Automonius-1.0-SNAPSHOT.jar"
+
+:: Run the JavaFX application from the JAR
+java --module-path %JAVAFX_PATH% --add-modules javafx.controls,javafx.fxml -jar %JAR_PATH%
+
 pause
